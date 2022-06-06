41d9d44e0dcfd8b8fce58634fe9b2dfcffe8c39e
Sean Pietersen <seanpietersen7@gmail.com>
2022-06-06 10:42:18 +0200

    Feat: Added Test

---
+++ DivideAndMaximize/DivideAndMaximize.Test/FindingTheGreatestSumFromDivideAndMultipleMethodTest.cs
@@ -0,0 +1,31 @@
+using System;
+using Xunit;
+
+namespace DivideAndMaximize.Test
+{
+    public class FindingTheGreatestSumFromDivideAndMultipleMethodTest
+    {
+        [Theory]
+        [InlineData(10, new long[] { 10 })]
+        [InlineData(13, new long[] { 3, 4 })]
+        [InlineData(50, new long[] { 6, 4, 2 })]
+        [InlineData(44, new long[] { 6, 4, 5 })]
+        [InlineData(46, new long[] { 1, 2, 3, 4, 5 })]
+        [InlineData(68, new long[] { 14, 9, 26 })]
+        [InlineData(371842558, new long[] { 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8 })]
+
+        public void DivideAndMultiplyMethodTest(long expected, long[] numbers)
+        {
+            //Arrange
+            IFindingTheGreatestSumFromDivideAndMultipleMethod findingTheGreatestSumFromDivideAndMultipleMethod = 
+                new FindingTheGreatestSumFromDivideAndMultipleMethod();
+
+            //Act
+            var actual = findingTheGreatestSumFromDivideAndMultipleMethod.DivideAndMultiplyMethod(numbers);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
